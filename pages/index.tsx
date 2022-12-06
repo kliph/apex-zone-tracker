87a96651f0331439fa587c4443e7a989acfccda3
Cliff Rodgers <kliph@users.noreply.github.com>
2022-12-06 09:29:33 -0500

    Allow locking ring location and placing separate zones

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,15 @@ import Image from "next/image";
 import React from "react";
 import bloodMoonMap from "../public/assets/blood-moon.png";
 
-const Menu = styled.div`
+const Menu = styled.menu`
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
+	position: absolute;
+	background-color: #223;
+	width: 100%;
+	z-index: 1;
+	padding: 0.25em;
+	margin: 0;
 `;
 
 const MapOverlay = styled.svg`
@@ -16,9 +22,10 @@ const MapOverlay = styled.svg`
 
 const MapContainer = styled.div`
   position: relative;
+	user-select: none;
 `;
 
-/* const RING_DIAMETERS = [279, 170, 103, 26, 20, 10]; */
+const RING_DIAMETERS = [279, 170, 103, 26, 20, 10];
 
 export default function Home(_props: any) {
   const [rings, setRings] = React.useState<any>([]);
@@ -29,24 +36,35 @@ export default function Home(_props: any) {
     mapContainer = element;
   };
 
-  function handleClick(evt: any) {
-    setRingNumber(ringNumber + 1);
+  function handlePlaceRing(evt: any) {
+    if (rings.length - 1 === ringNumber) {
+      rings.pop()
+    };
     setRings([
+      ...rings,
       <circle
         cx={evt.clientX - mapContainer!.offsetLeft}
         cy={evt.clientY - mapContainer!.offsetTop}
-        r="279"
+        r={RING_DIAMETERS[ringNumber]}
         stroke="red"
         strokeWidth="2"
         fill="none"
+        key={ringNumber}
       />,
     ]);
   }
 
+  function handleSaveRingPosition() {
+    setRingNumber(ringNumber + 1);
+  }
+
   return (
     <>
-      <Menu>Here's what's on the menu</Menu>
-      <MapContainer onClick={handleClick} ref={setMapContainerRef}>
+      <Menu>
+        <div>Current Zone: {ringNumber}</div>
+        <button onClick={handleSaveRingPosition}>Save Ring Position</button>
+      </Menu>
+      <MapContainer onClick={handlePlaceRing} ref={setMapContainerRef}>
         <MapOverlay width="1012" height="1013">
           {rings}
         </MapOverlay>
